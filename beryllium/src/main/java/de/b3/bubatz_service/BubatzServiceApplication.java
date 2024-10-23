74ebec3d28461c93e80df07d216ff63ceb262d3b
Adrian John <adrianjohn6543@gmail.com>
2024-10-23 10:23:01 +0200

    feat: add the spring backend project

---
+++ beryllium/src/main/java/de/b3/bubatz_service/BubatzServiceApplication.java
@@ -0,0 +1,13 @@
+package de.b3.bubatz_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubatzServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BubatzServiceApplication.class, args);
+	}
+
+}
---
+++ beryllium/src/test/java/de/b3/bubatz_service/BubatzServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.b3.bubatz_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BubatzServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
