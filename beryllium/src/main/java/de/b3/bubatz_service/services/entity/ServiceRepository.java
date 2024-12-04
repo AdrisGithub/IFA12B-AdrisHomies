bcd2a936b0c4424e3344b0f919cb8d115bb2209a
Adrian John <adrianjohn6543@gmail.com>
2024-12-04 11:58:58 +0100

    feat: add service repository

---
+++ beryllium/src/main/java/de/b3/bubatz_service/services/entity/ServiceRepository.java
@@ -0,0 +1,6 @@
+package de.b3.bubatz_service.services.entity;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface ServiceRepository extends ListCrudRepository<Service, Integer> {
+}
