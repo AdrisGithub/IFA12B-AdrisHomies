5da09d74c0e4c8644eba87fb270687061259853a
Adrian John <adrianjohn6543@gmail.com>
2024-12-04 10:52:06 +0100

    feat: add the missing properties

--- beryllium/src/main/java/de/b3/bubatz_service/articles/entity/ArticleItem.java
+++ beryllium/src/main/java/de/b3/bubatz_service/articles/entity/ArticleItem.java
@@ -3,6 +3,9 @@ package de.b3.bubatz_service.articles.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 @Entity
 @Table(name = "ARTICLE_ITEMS")
 @Data
@@ -10,13 +13,34 @@ public class ArticleItem {
 
     @Id
     @GeneratedValue
-    private Integer articleId;
+    private Integer articleItemId;
 
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "articleId", referencedColumnName = "spotId")
+    @JoinColumn(name = "spotId", referencedColumnName = "spotId")
     private DepositorySpot spot;
 
     @ManyToOne
     @JoinColumn(name = "articleId", nullable = false,insertable = false, updatable = false)
     private Article article;
+
+    @Column(precision = 4, scale = 2)
+    private BigDecimal buyPrice;
+
+    @Column(precision = 4, scale = 2)
+    private BigDecimal sellPrice;
+
+    @Column(precision = 3)
+    private BigDecimal amount;
+
+    public BigDecimal getBuyPrice() {
+        return buyPrice.setScale(2, RoundingMode.CEILING);
+    }
+
+    public BigDecimal getSellPrice() {
+        return sellPrice.setScale(2, RoundingMode.CEILING);
+    }
+
+    public BigDecimal getAmount() {
+        return amount.setScale(0, RoundingMode.CEILING);
+    }
 }
