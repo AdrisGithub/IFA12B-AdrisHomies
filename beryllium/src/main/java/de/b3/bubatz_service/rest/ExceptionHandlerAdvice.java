9a55654850b1572bfb80bb8544d9b7c1991fcb2d
Adrian John <adrianjohn6543@gmail.com>
2024-12-06 12:10:51 +0100

    feat: add error logging and tidy up the error message

--- beryllium/src/main/java/de/b3/bubatz_service/rest/ExceptionHandlerAdvice.java
+++ beryllium/src/main/java/de/b3/bubatz_service/rest/ExceptionHandlerAdvice.java
@@ -2,6 +2,7 @@ package de.b3.bubatz_service.rest;
 
 import de.b3.bubatz_service.generated.models.Error;
 import de.b3.bubatz_service.services.exceptions.InvalidAdditionalValuesException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,9 +11,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
+@Slf4j
 public class ExceptionHandlerAdvice {
 
-    public static final String INVALID_ADD_VALUES_ERROR = "Bei der Speicherung der Entität mit der Id %s ist ein Fehler entstanden";
+    public static final String INVALID_ADD_VALUES_ERROR = "Beim Einlesen der Entität mit der Id %s ist ein Fehler entstanden";
 
     @ExceptionHandler(value = {InvalidAdditionalValuesException.class})
     public ResponseEntity<Error> resourceNotFoundException(InvalidAdditionalValuesException ex) {
@@ -22,6 +24,7 @@ public class ExceptionHandlerAdvice {
         error.setMessage("Invalide Entität");
         error.setDetail(String.format(INVALID_ADD_VALUES_ERROR, ex.getId()));
 
+        log.error("{}",error);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(error);
