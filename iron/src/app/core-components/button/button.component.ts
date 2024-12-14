73b63e945c11101911ccc64916323c27e7f1b77a
Joschka Röser <joschka@startmail.com>
2024-12-14 21:06:16 +0100

    feat: rework button component

--- iron/src/app/core-components/button/button.component.ts
+++ iron/src/app/core-components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, output } from '@angular/core';
 
 @Component({
   selector: 'ls-button',
@@ -6,20 +6,26 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [],
   template: `
   
-    <button type="button">
-      <ng-content/>
+    <button type="button" (click)="onClick.emit()">
+      <ng-content/> 
     </button>
 
   `,
   styles: `
   
     button {
-      all: unset;
-      margin: 1em;
-      background-color: #DE8315;
+      padding: .3em .8em;
+      background-color: var(--button);
+      color: var(--text);
+      border: none;
+      border-radius: 10px;
+      font-size: 24px;
+      cursor: pointer;
     }
 
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ButtonComponent { }
+export class ButtonComponent { 
+  onClick = output();
+}
