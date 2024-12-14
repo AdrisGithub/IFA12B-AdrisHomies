a5946049ce7a5046462479ff6c83b20823fdebf6
Joschka Röser <joschka@startmail.com>
2024-12-14 21:02:15 +0100

    feat: create base modal component

---
+++ iron/src/app/core-components/modal-container/modal-container.component.ts
@@ -0,0 +1,29 @@
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ModalService } from '../../services/Modal.service';
+
+@Component({
+  selector: 'ls-modal-container',
+  standalone: true,
+  imports: [],
+  template: `
+  
+  <section class="modal">
+    <header>
+      <h1>{{ title() }}</h1>
+      <button (click)="closeDialoge()" type="button">X</button>
+    </header>
+    <ng-content/>
+  </section>
+  
+  `,
+  styleUrl: './modal-container.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ModalContainerComponent {
+
+  title = input.required<string>();
+
+  modalService = inject(ModalService);
+
+  closeDialoge = () => { this.modalService.closeModal(); }
+ }
