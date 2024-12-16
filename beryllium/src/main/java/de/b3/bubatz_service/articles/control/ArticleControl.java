2085f10c3bf6a3295e9f5dc9b83d59468fe43340
just-an-overthinking-boi <timseidler@yahoo.de>
2024-12-16 12:06:21 +0100

    chore: annoying import stuff

--- beryllium/src/main/java/de/b3/bubatz_service/articles/control/ArticleControl.java
+++ beryllium/src/main/java/de/b3/bubatz_service/articles/control/ArticleControl.java
@@ -7,6 +7,7 @@ import de.b3.bubatz_service.articles.util.ArticleItemMapper;
 import de.b3.bubatz_service.articles.util.ArticleMapper;
 import de.b3.bubatz_service.generated.models.GetArticle;
 import jakarta.persistence.EntityNotFoundException;
+import de.b3.bubatz_service.generated.models.PatchArticle;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -26,7 +27,7 @@ public class ArticleControl {
                 .toList();
     }
 
-    public GetArticle patchArticle(de.b3.bubatz_service.generated.models.PatchArticle patchArticle) {
+    public GetArticle patchArticle(PatchArticle patchArticle) {
 
         Article article = repository.findById(patchArticle.getId()).orElseThrow(
                 () -> new EntityNotFoundException("Article with id " + patchArticle.getId() + " not found")
