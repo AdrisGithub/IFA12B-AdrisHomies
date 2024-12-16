03de98cf63115e7e7daab596529548d131a7bc3d
Adriiii <116658066+AdrisGithub@users.noreply.github.com>
2024-12-16 08:54:38 +0100

    fix: add Cors Policy to make http calls possible (#59)

--- beryllium/src/main/java/de/b3/bubatz_service/rest/ServiceController.java
+++ beryllium/src/main/java/de/b3/bubatz_service/rest/ServiceController.java
@@ -8,12 +8,14 @@ import de.b3.bubatz_service.services.control.ServiceControl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class ServiceController implements ServiceApi {
 
     private final ServiceControl control;
