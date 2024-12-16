198c61880755d82ec2c951a921ba2cf7f3fa0af2
B3EA <Emre.Aral@schueler.6072-fuerth.de>
2024-12-16 14:51:26 +0100

    chore: end of day commit

--- iron/src/app/modals-ucs/ArtikelEinlagern/ArtikelEinlagern.component.ts
+++ iron/src/app/modals-ucs/ArtikelEinlagern/ArtikelEinlagern.component.ts
@@ -1,16 +1,31 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ModalContainerComponent } from "../../core-components/modal-container/modal-container.component";
 import { ModalBase } from '../../services/Modal.service';
+import {Article, ArticleComponent} from '../../core-components/article/article.component';
+import {BorderContainerComponent} from '../../core-components/BorderContainer/BorderContainer.component';
 
 @Component({
   selector: 'ls-artikel-einlagern',
   standalone: true,
-  imports: [ModalContainerComponent],
+  imports: [ModalContainerComponent, ArticleComponent, BorderContainerComponent],
   template: `
-  
-  <ls-modal-container [title]="'Artikel einlagern'">
-  </ls-modal-container>
-  
+
+    <ls-modal-container [title]="'Artikel einlagern'">
+      <ls-article [article]="article"></ls-article>
+      <div class="container">
+        <ls-border-container [title]="'im Lager'">
+          <ul>
+            <li>Reihe 5 - Platz 10</li>
+            <li>Reihe 2 - Platz 8</li>
+            <li>Reihe 29 - Platz 31</li>
+          </ul>
+        </ls-border-container>
+        <form>
+          <input type="text">
+          <input type="text">
+        </form>
+      </div>
+    </ls-modal-container>
   `,
   styles: `
     :host {
@@ -19,4 +34,11 @@ import { ModalBase } from '../../services/Modal.service';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ArtikelEinlagernComponent implements ModalBase { }
+export class ArtikelEinlagernComponent implements ModalBase {
+  article: Article = {
+    title: "Grünes T-Shirt",
+    price: "3,00€",
+    amountOrdered: 5,
+    amountWarehouse: 28
+  }
+}
