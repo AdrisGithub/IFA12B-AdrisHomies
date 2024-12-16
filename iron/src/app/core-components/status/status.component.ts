124c38a5c13b8d41e0075729812ab7a0775523ba
B3EA <Emre.Aral@schueler.6072-fuerth.de>
2024-12-16 11:11:38 +0100

    feat: finish status component

--- iron/src/app/core-components/status/status.component.ts
+++ iron/src/app/core-components/status/status.component.ts
@@ -4,11 +4,17 @@ import {Component, input} from '@angular/core';
   selector: 'ls-status',
   standalone: true,
   imports: [],
-  templateUrl: './status.component.html',
+  template:`
+    <div class="container">
+      <div class="led" [class]="statusColour()"></div>
+      <span>{{ amount() }} {{ displayText() }}</span>
+    </div>
+  `,
   styleUrl: './status.component.css'
 })
 export class StatusComponent {
-  statusType = input.required<string>(); //TODO enum oderso
+  statusColour = input.required<StatusColour>(); //TODO enum oderso
   displayText = input<string>();
   amount = input<number>();
 }
+export type StatusColour = "red" | "green" | "orange";
