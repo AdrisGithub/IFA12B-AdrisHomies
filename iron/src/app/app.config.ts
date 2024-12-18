6251f9c11bea66ddbdf1c60bac85e4c3632982de
Adriiii <116658066+AdrisGithub@users.noreply.github.com>
2024-12-18 20:07:50 +0100

    fix: change the schema to https

--- iron/src/app/app.config.ts
+++ iron/src/app/app.config.ts
@@ -6,7 +6,7 @@ import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({eventCoalescing: true}),  {
     provide: BASE_PATH,
-    useValue: 'http://segitztherme.de:8080'
+    useValue: 'https://segitztherme.de:8080'
   },
     {
       provide: LOCALE_ID, useValue: 'de'
