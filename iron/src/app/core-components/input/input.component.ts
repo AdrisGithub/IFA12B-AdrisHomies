4ba3cccc1f97058cec523751643f28ba9a4e6362
Adriiii <116658066+AdrisGithub@users.noreply.github.com>
2024-12-18 14:16:05 +0100

    feat/putinarticle (#93)
    * chore: end of day commit
    
    * fix: did some stuff that did some stuff
    
    * feat: add stash article modal
    
    * chore: end of day commit (nothing works)
    
    * feat: add fullWidth-Mode for Button
    
    * fix: add margin to modal heading
    
    * feat: add computed signal to store
    
    * feat: add layout of atricle details modal
    
    * resolve merge conflict
    
    * feat: add article component with nonworking button
    
    * ich kann das alles nicht mehr
    
    * feat: integrate the current focus
    
    * feat: integrate putin with the store
    
    * style: input field
    
    ---------
    
    Co-authored-by: B3EA <Emre.Aral@schueler.6072-fuerth.de>
    Co-authored-by: Joschka Röser <joschka@startmail.com>
    Co-authored-by: Schmoni <simone.endres@gmx.de>

--- iron/src/app/core-components/input/input.component.ts
+++ iron/src/app/core-components/input/input.component.ts
@@ -1,4 +1,4 @@
-import {Component, input} from '@angular/core';
+import {Component, input, output} from '@angular/core';
 import {NgStyle} from '@angular/common';
 
 @Component({
@@ -14,14 +14,20 @@ import {NgStyle} from '@angular/common';
       }
       <span [ngStyle]="{'font-size': fontSize()+'px'}" >{{ displayText() }}</span>
     </div>
-    <input type="text">
+    <input type="text" (input)="testInput($event)">
   `,
   styleUrl: './input.component.css'
 })
 export class InputComponent {
   icon = input<IconType>();
-  displayText = input.required<String>();
+  displayText = input.required<string>();
   fontSize =input(26)
+  value = output<string>()
+
+
+  testInput = (event: any) => {
+    this.value.emit(event.target.value)
+  }
 }
 
 export type IconType = "ladders" | "x" | "container";
