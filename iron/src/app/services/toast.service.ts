055b5ca3e806ecf5087ccab5f693606b33302cba
Adriiii <adrianjohn6543@gmail.com>
2024-12-19 13:13:47 +0100

    feat: add toast service prototype

---
+++ iron/src/app/services/toast.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject, Observable, tap} from 'rxjs';
+import {Toast} from '../core-components/toasts/toast';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+
+  private toasts: BehaviorSubject<Toast[]> = new BehaviorSubject<Toast[]>([]);
+
+
+  addToast(toast: Toast){
+    const toasts = this.toasts.value;
+    toasts.push(toast)
+    this.toasts.next(toasts)
+  }
+
+  getToasts(): Observable<Toast[]> {
+    return this.toasts.asObservable()
+      .pipe(
+        tap(toasts => {
+            toasts.forEach(toast => setTimeout(() => {this.removeToast(toast.id)},5000));
+            return toasts;
+          }
+        )
+      );
+  }
+
+  removeToast(toastId: number){
+    const toasts = this.toasts.value.filter(toast => toast.id === toastId);
+    this.toasts.next(toasts);
+  }
+
+
+}
