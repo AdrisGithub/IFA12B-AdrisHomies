ee56d00e6efcc1f3d8e41e5a6a6655916c62a34f
Joschka Röser <joschka@startmail.com>
2024-12-19 12:52:16 +0100

    feat: did a lot of things

--- iron/src/app/modals-ucs/NeuerArtikel/NeuerArtikel.component.ts
+++ iron/src/app/modals-ucs/NeuerArtikel/NeuerArtikel.component.ts
@@ -3,6 +3,7 @@ import { ModalBase } from '../../services/Modal.service';
 import { ModalContainerComponent } from '../../core-components/modal-container/modal-container.component';
 import { BorderContainerComponent } from '../../core-components/BorderContainer/BorderContainer.component';
 import { InputComponent } from '../../core-components/input/input.component';
+import { Pair } from '../Artikeldetails/Artikeldetails.component';
 InputComponent
 
 @Component({
@@ -14,64 +15,55 @@ InputComponent
   <ls-modal-container title="Neuen Artikel anlegen">
     <div class="grid-container">
       <ls-border-container title="Grunddaten">
-        <ls-input [fontSize]="20" displayText="Name" />
-        <ls-input [fontSize]="20" displayText="Preis" />
-        <ls-input [fontSize]="20" displayText="Beschreibung" />
-      </ls-border-container>
-      <ls-border-container class="container" title="Artikelinfos">
-        <div class="info">
-          <input (input)="manageInputChangeEvent($event)" type="text">
-          <span>🡪</span>
-          <input type="text" />
+        <div class="grunddaten">
+          <ls-input class="input-component" [fontSize]="20" displayText="Name:" />
+          <ls-input class="input-component" [fontSize]="20" displayText="Menge:" />
+          <ls-input class="input-component" [fontSize]="20" displayText="Preis:" />
+          <ls-input class="input-component" [fullHeight]="true" [fontSize]="20" displayText="Beschreibung:" />
         </div>
+      </ls-border-container>
+      <ls-border-container title="Artikelinfos">
+        <div class="info-container">
+          @for (inp of inputs(); track $index; let idx = $index) {
+            <div class="info">
+              <input (input)="manageInputChangeEvent('key', idx, $event)" type="text">
+              <img class="arrow" src="./arrow.svg">
+              <input (input)="manageInputChangeEvent('value', idx, $event)" type="text">
+              <img class="close-icon" src="./x.svg" alt="">
+            </div>
+          }
+        </div>
+        <button (click)="addInp()">+</button>
       </ls-border-container>
     </div>
   </ls-modal-container>
   
   `,
-  styles: `
-    :host {
-      display: block;
-    }
-
-    .grid-container {
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-      gap: 15px;
-    }
-
-    input {
-      border: 1px solid var(--text);
-      background-color: var(--card-bg);
-      border-radius: 10px;
-      padding: 0.5em;
-      margin-bottom: 1em;
-      color: var(--text);
-      width: 100%;
-    }
-
-    input:focus{
-      outline: none;
-    }
-
-    .info {
-      display: grid;
-      grid-template-columns: 1fr auto 1fr
-    }
-    
-    span {
-      margin: auto 10px;
-    }
-
-  `,
+  styleUrl: './NeuerArtikel.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NeuerArtikelComponent implements ModalBase {
 
-  inputs = signal<string>("");
+  inputs = signal<Pair[]>([ { key: '', value: '' } ]);
 
-  manageInputChangeEvent = (event: any) => {
-    this.inputs.set(event.target.value)
+  manageInputChangeEvent = (type: "key" | "value", index: number, event: any) => {
+    this.inputs.update(pairs => {
+      const newPairs = [...pairs];
+      let currentPair = {...newPairs[index]};
+
+      currentPair[type] = event.target.value;
+
+      newPairs[index] = currentPair;
+      return newPairs;
+    })
+  }
+
+  addInp = () => {
+    this.inputs.update(pairs => {
+      const newPairs = [...pairs];
+      newPairs.push({ key: '', value: '' })
+      return newPairs;
+    })
   }
 
   eff = effect(() => console.log(this.inputs()))
