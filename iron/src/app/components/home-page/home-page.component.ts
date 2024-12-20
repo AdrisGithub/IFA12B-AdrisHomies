72ed4bde54458025e0709cb8d328587866dbba14
Joschka Röser <79374471+Uzade@users.noreply.github.com>
2024-12-20 07:02:44 +0000

    feat: create basic layout of new modal (#102)
    * feat: create basic layout of new modal
    
    * feat: allow input to take full height
    
    if fullHeight set to true
    
    * feat: update typescript language level
    
    to newest version
    
    * feat: add new arrow icon
    
    * feat: make pair type publically accessible
    
    * feat: did a lot of things
    
    * feat: add some styling and close logic to zusatzinfos
    
    * feat: send new article to backend
    
    * fix: remove logs
    
    ---------
    
    Co-authored-by: Adrian John <adrianjohn6543@gmail.com>
    Co-authored-by: Adriiii <116658066+AdrisGithub@users.noreply.github.com>

--- iron/src/app/components/home-page/home-page.component.ts
+++ iron/src/app/components/home-page/home-page.component.ts
@@ -3,8 +3,9 @@ import {BorderContainerComponent} from '../../core-components/BorderContainer/Bo
 import {ArticleComponent} from '../../core-components/article/article.component';
 import {BubatzStore} from '../../store/ls-store';
 import {ServiceComponent} from '../../core-components/service/service.component';
-import {ButtonComponent} from '../../core-components/button/button.component';
-import {ModalService} from '../../services/Modal.service';
+import { ButtonComponent } from "../../core-components/button/button.component";
+import { ModalService } from '../../services/Modal.service';
+import { NeuerArtikelComponent } from '../../modals-ucs/NeuerArtikel/NeuerArtikel.component';
 import {ArtikelVerkaufenComponent} from '../../modals-ucs/ArtikelVerkaufen/ArtikelVerkaufen.component';
 import {ArtikelNachbestellenComponent} from '../../modals-ucs/ArtikelNachbestellen/ArtikelNachbestellen.component';
 
@@ -16,7 +17,7 @@ import {ArtikelNachbestellenComponent} from '../../modals-ucs/ArtikelNachbestell
     ArticleComponent,
     ServiceComponent,
     ButtonComponent
-  ],
+],
   template: `
     <header class="flex">
       <img src="logo.png" alt="Logo der Segitz-Therme">
@@ -28,14 +29,14 @@ import {ArtikelNachbestellenComponent} from '../../modals-ucs/ArtikelNachbestell
     </header>
     <div class="home-container flex">
       <ls-border-container [title]="'Waren'">
-          <ls-button [fullWidth]="true" (onClick)="createArticle()">neuen Artikel anlegen</ls-button>
-          <div class="article-items">
-            @for (article of articles(); track article.id) {
-              <div class="item">
-                <ls-article [article]="article"></ls-article>
-              </div>
-            }
-          </div>
+        <ls-button [fullWidth]="true" (onClick)="openNewArticleModal()">neuen Artikel anlegen</ls-button>
+        <div class="article-items">
+          @for (article of articles(); track article.id) {
+            <div class="item">
+              <ls-article [article]="article"></ls-article>
+            </div>
+          }
+        </div>
       </ls-border-container>
       <ls-border-container [title]="'Dienstleistungen'">
           <ls-button [fullWidth]="true" (onClick)="createService()">neue Dienstleistung anlegen</ls-button>
@@ -52,14 +53,13 @@ import {ArtikelNachbestellenComponent} from '../../modals-ucs/ArtikelNachbestell
   styleUrl: 'home-page.component.css',
 })
 export class HomePageComponent {
-  modalService = inject(ModalService);
   store = inject(BubatzStore);
+  modalService = inject(ModalService);
+
   articles = this.store.getMappedArticles;
   services = this.store.getMappedServices;
 
-  createArticle() {
-    this.modalService.openModal(ArtikelVerkaufenComponent)
-  }
+  openNewArticleModal = () => { this.modalService.openModal(NeuerArtikelComponent); }
 
   createService() {
     this.modalService.openModal(ArtikelNachbestellenComponent)
---
+++ iron/src/app/modals-ucs/NeuerArtikel/NeuerArtikel.component.ts
@@ -0,0 +1,104 @@
+import { ChangeDetectionStrategy, Component, effect, inject, signal } from '@angular/core';
+import { ModalBase } from '../../services/Modal.service';
+import { ModalContainerComponent } from '../../core-components/modal-container/modal-container.component';
+import { BorderContainerComponent } from '../../core-components/BorderContainer/BorderContainer.component';
+import { InputComponent } from '../../core-components/input/input.component';
+import { Pair } from '../Artikeldetails/Artikeldetails.component';
+import { BubatzStore } from '../../store/ls-store';
+import { ButtonComponent } from '../../core-components/button/button.component';
+
+@Component({
+  selector: 'ls-neuer-artikel',
+  standalone: true,
+  imports: [ModalContainerComponent, BorderContainerComponent, InputComponent, ButtonComponent],
+  template: `
+  
+  <ls-modal-container title="Neuen Artikel anlegen">
+    <div class="grid-container">
+      <ls-border-container class="grunddaten-border" title="Grunddaten">
+        <div class="grunddaten">
+          <ls-input class="input-component" (value)="name.set($event)" [fontSize]="20" displayText="Name:" />
+          <ls-input class="input-component" (value)="menge.set($event)" [fontSize]="20" displayText="Menge:" />
+          <ls-input class="input-component" (value)="preis.set($event)" [fontSize]="20" displayText="Preis:" />
+          <ls-input class="input-component" (value)="beschreibung.set($event)" [fullHeight]="true" [fontSize]="20" displayText="Beschreibung:" />
+        </div>
+      </ls-border-container>
+      <ls-border-container title="Artikelinfos">
+        <div class="info-container">
+          @for (inp of inputs(); track $index; let idx = $index) {
+            <div class="info">
+              <input (input)="manageInputChangeEvent('key', idx, $event)" [value]="inp.key" type="text">
+              <img class="arrow" src="./arrow.svg" alt="arrow icon">
+              <input (input)="manageInputChangeEvent('value', idx, $event)" [value]="inp.value" type="text">
+              <img (click)="removeInput(idx)" class="close-icon" src="./x.svg" alt="remove icon">
+            </div>
+          }
+        </div>
+        <div class="button-container">
+          <button (click)="addInp()">
+            <img class="plus" src="./plus-sign.svg" alt="plus icon">
+            <span class="plus-text">Weitere Info</span>
+          </button>
+        </div>
+      </ls-border-container>
+      <ls-button [fullWidth]="true" class="save-button" (onClick)="this.saveArticle()">Anlegen</ls-button>
+    </div>
+  </ls-modal-container>
+  
+  `,
+  styleUrl: './NeuerArtikel.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NeuerArtikelComponent implements ModalBase {
+
+  store = inject(BubatzStore);
+
+  name = signal<string>('');
+  menge = signal<string>('');
+  preis = signal<string>('');
+  beschreibung = signal<string>('');
+
+  inputs = signal<Pair[]>([ { key: '', value: '' } ]);
+
+  manageInputChangeEvent = (type: "key" | "value", index: number, event: any) => {
+    this.inputs.update(pairs => {
+      const newPairs = [...pairs];
+      let currentPair = {...newPairs[index]};
+
+      currentPair[type] = event.target.value;
+
+      newPairs[index] = currentPair;
+      return newPairs;
+    })
+  }
+
+  addInp = () => {
+    this.inputs.update(pairs => {
+      const newPairs = [...pairs];
+      newPairs.push({ key: '', value: '' })
+      return newPairs;
+    })
+  }
+
+  removeInput = (idx: number) => {
+    this.inputs.update(pairs => pairs.toSpliced(idx, 1))
+  }
+
+  saveArticle = () => {
+    this.store.createArticle({
+      name: this.name(),
+      amount: parseInt(this.menge()),
+      buyPrice: 69,
+      sellPrice: parseFloat(this.preis()),
+      description: this.beschreibung(),
+      infos: this.mapInfos(this.inputs())
+
+    })
+  } 
+
+  mapInfos = (pairs: Pair[]): { [key: string]: string }  => {
+    const infos: { [key: string]: string } = {}
+    pairs.forEach(pair => infos[pair.key] = pair.value)
+    return infos;
+  }
+}
