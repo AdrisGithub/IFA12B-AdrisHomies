a2ef167a2f26192d1517e58e352617cdddf8efc6
Tim Seidler <timseidler@yahoo.de>
2024-12-20 11:47:44 +0100

    ref: depositoryspots are now party of the item entity instead of a standalone entity (#116)

--- beryllium/src/main/java/de/b3/bubatz_service/articles/db/ArticleItemRepository.java
+++ beryllium/src/main/java/de/b3/bubatz_service/articles/db/ArticleItemRepository.java
@@ -4,4 +4,5 @@ import de.b3.bubatz_service.articles.db.entity.ArticleItemEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
 public interface ArticleItemRepository extends ListCrudRepository<ArticleItemEntity, Integer> {
+    boolean findDepositorySpotByColumnNrAndRowNr(Integer spaltenNr, Integer reihenNr);
 }
